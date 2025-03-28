93fc2a9e3d304c3987c1eaa6c0954bf913a10efb
Arinelson Santos <48639869+arinelson@users.noreply.github.com>
2025-03-28 18:36:15 -0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 
 // Armazena a URL do Google Sheets
 // IMPORTANTE: Este arquivo deve ser incluído no .gitignore manualmente
-export const GOOGLE_SHEETS_URL: string = "";
+export const GOOGLE_SHEETS_URL: string = "https://script.google.com/macros/s/AKfycbzn0oGRz1xD2di7Q6DgW047sW8Cr49LUApET2w9yfUjCT9DsHSh2fOFffyRRuxRWTiPJg/exec";
